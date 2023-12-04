6daf33dc11f63e602d2f6aafac1eec5758465649
shemaabra <abrashema9@gmail.com>
2023-12-04 14:04:17 +0300

    access mutator done

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -44,6 +45,13 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    protected function name():Attribute
+    {
+        return Attribute::make(
+            get: fn(string $value) => Str::upper($value)
+        );
+    }
 
     protected function password():Attribute
     {
